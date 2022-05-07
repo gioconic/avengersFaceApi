3615ef605e08222b58e0bdaef3e2ef1bb35d2214
Athena <giovanni.gab@icloud.com>
2022-05-07 02:02:10 +0200

    canvas drawn over image

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ Promise.all([
 ]).then(start);
 
 function start() {
+  // Creating a container
+  const container = document.createElement("div");
+  container.style.position = "relative";
+  document.body.append(container);
+
   // To know when the page finishes loading all the models
   document.body.append("Models fully loaded");
 
@@ -15,17 +20,39 @@ function start() {
     const image = await faceapi.bufferToImage(imageUpload.files[0]);
 
     // To display the image
-    document.body.append(image);
+    container.append(image);
+
+    // Creating and adding the canvas from the image we passed to it
+    const canvas = faceapi.createCanvasFromMedia(image);
+    // Resize the canvas to the correct size
+    container.append(canvas);
+    // image and canvas appended to the container, so they're both absolutely positioned on top of each other perfectly inside the relative container
+
+    const displaySize = { width: image.width, height: image.height };
+
+    // Resize our canvas
+    faceapi.matchDimensions(canvas, displaySize);
 
     // Detect all faces
     // withFaceLandmarks -> to detect where the faces are
     // withFaceDescriptors -> to draw the boxes and such around the faces
-    const detection = await faceapi
+    const detections = await faceapi
       .detectAllFaces(image)
       .withFaceLandmarks()
       .withFaceDescriptors();
 
+    // Resize all the boxes for our detection to be the correct size based on the sizes we pass it
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+
+    resizedDetections.forEach((detection) => {
+      // Box of the face that our algorithm determines for our individual faces in our image
+      const box = detection.detection.box;
+      // Draw the boxes onto the screen
+      const drawBox = new faceapi.draw.DrawBox(box, { label: "Face" });
+      drawBox.draw(canvas);
+    });
+
     // Counts how many faces are and show the number
-    document.body.append(detection.length);
+    /* document.body.append(detection.length); */
   });
 }
